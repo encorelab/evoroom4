ae78f395ae8ad0e2af8e0273dff51002c0f310f1
Armin Krauss <armin.krauss@utoronto.ca>
2013-03-11 00:10:55 -0400

    Setting up Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+    jshint: {
+      all: ['Gruntfile.js', 'mobile/js/ck.mobile.view.js', 'mobile/js/ck.mobile.js']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  // grunt.loadNpmTasks('grunt-coffee');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint']);
+  
+
+};
