d6c539eb704a8c592962077883398d1e488d9886
Matt Zukowski <matt@zukowski.ca>
2013-03-26 02:11:52 -0400

    show objectives for teacher for Rotation I and Meetup I

--- js/evoroom.teacher.js
+++ js/evoroom.teacher.js
@@ -74,6 +74,12 @@ window.EvoRoom.Teacher = function () {
     var phases = new EvoRoom.Model.Phases();
     phases.fetch().done(function () {
       app.phase = phases.first();
+
+      if (!app.phase) {
+        console.error("No phase document found in /phases!");
+        throw "EXPLOSION!";
+      }
+
       app.phase.on('change', app.phaseChanged);
       app.phase.wake(app.config.wakeful.url);
 
@@ -86,9 +92,30 @@ window.EvoRoom.Teacher = function () {
       });
 
       app.explanations = new EvoRoom.Model.Explanations();
+      app.explanations.wake(app.config.wakeful.url);
       app.explanations.on('change add', app.explanationChanged);
       app.explanations.on('reset', function(explanations) { explanations.each(app.explanationChanged); });
       app.users.fetch();
+
+      app.observations = new EvoRoom.Model.Observations();
+      app.observations.wake(app.config.wakeful.url);
+      app.observations.on('change add', function (ob) {
+        app.userChanged(app.lookupUserByUsername(ob.get('username'))); 
+      });
+      app.observations.on('change add reset', function () {
+        jQuery('#obs-count .count').text(app.observations.length); 
+      });
+      app.observations.fetch();
+
+      app.notes = new EvoRoom.Model.Notes();
+      app.notes.wake(app.config.wakeful.url);
+      app.notes.on('change add', function (no) { 
+        app.userChanged(app.lookupUserByUsername(no.get('username'))); 
+      });
+      app.notes.on('change add reset', function () {
+        jQuery('#notes-count .count').text(app.notes.length); 
+      });
+      app.notes.fetch();
     });
   };
   
@@ -211,10 +238,22 @@ window.EvoRoom.Teacher = function () {
       .addClass('current');
 
     app.users.each(function (u) {
-      app.userChanged(u);
+      app.updateStudentMarkerForUser(u);
+    });
+
+    jQuery('tr.phase').each(function () {
+      var tr = jQuery(this);
+      var trPhaseDef = app.lookupPhaseDefinitionByName(tr.data('phase'));
+      if (trPhaseDef.number <= app.phase.get('phase_number')) {
+        tr.find('.buttons button').addClass('teacher-button-done');
+        tr.find('.buttons .objective').show();
+      } else {
+        tr.find('.buttons .objective').hide();
+      }
     });
 
     app.updateOpenInquiryButton();
+    app.updatePhaseReady();
   };
 
   app.studentMarker = function (username) {
@@ -225,7 +264,7 @@ window.EvoRoom.Teacher = function () {
     return marker;
   };
 
-  app.userChanged = function (user) {
+  app.updateStudentMarkerForUser = function (user) {
     var username = user.get('username');
     var phaseName = user.get('user_phase') || app.phase.get('phase_name');
     var marker = app.studentMarker(username);
@@ -244,8 +283,13 @@ window.EvoRoom.Teacher = function () {
     } else {
       marker.removeClass('ready');
     }
+  };
 
+  app.userChanged = function (user) {
+    app.updateStudentMarkerForUser(user);
     app.updatePhaseReady();
+
+    app.studentMarker(user.get('username')).effect('highlight');
   };
 
   app.explanationChanged = function (exp) {
@@ -264,12 +308,12 @@ window.EvoRoom.Teacher = function () {
     jQuery('button.start-phase')
       .removeClass('teacher-button-primed ready');
 
-    if (app.users.length > 0 && app.phase.get('phase_number') < 1) {
-      jQuery('.phase-rotation_1 button.start-phase')
-        .removeClass('teacher-button-faded')
-        .removeClass('teacher-button-done')
-        .addClass('teacher-button-primed ready');
-    }
+    // if (app.users.length > 0 && app.phase.get('phase_number') < 1) {
+    //   jQuery('.phase-rotation_1 button.start-phase')
+    //     .removeClass('teacher-button-faded')
+    //     .removeClass('teacher-button-done')
+    //     .addClass('teacher-button-primed ready');
+    // }
 
     if (app.users.all(function (u) { return u.maxPhaseCompleted() >= app.phase.get('phase_number'); })) {
       jQuery('.phase-'+(app.phase.get('phase_number')+1)+' button.start-phase')
